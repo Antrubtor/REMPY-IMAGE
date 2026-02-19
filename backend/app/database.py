5b24bdae9a190f072612f123cf39dbdbe6d0c73f
Antrubtor <antrubtor@gmail.com>
2026-02-19 14:34:06 +0100

    create all endpoints

--- backend/app/database.py
+++ backend/app/database.py
@@ -17,25 +17,107 @@ def connect() -> tuple[psycopg.Connection | None, psycopg.Cursor | None]:
 def create_table() -> None:
     conn, cur = connect()
     try:
-         cur.execute("""
-            CREATE TABLE IF NOT EXISTS benchmarks (
-                id SERIAL PRIMARY KEY,
-                image_hash VARCHAR(64) UNIQUE NOT NULL,
-                image_result BYTEA
-            )
-        """)
-
-         cur.execute("""
-             CREATE TABLE IF NOT EXISTS benchmarks_times (
-                id SERIAL PRIMARY KEY,
-                benchmark_id INTEGER NOT NULL REFERENCES benchmarks(id) ON DELETE CASCADE,
-                time_python DOUBLE PRECISION NOT NULL,
-                time_numba DOUBLE PRECISION NOT NULL
-             )
-            """)
+        cur.execute("""
+                    CREATE TABLE IF NOT EXISTS benchmarks (
+                        id SERIAL PRIMARY KEY,
+                        image_hash VARCHAR(64) UNIQUE NOT NULL,
+                        image_result DOUBLE PRECISION[][] NOT NULL
+                    )
+                    """)
+        cur.execute("""
+                    CREATE TABLE IF NOT EXISTS benchmarks_times (
+                        id SERIAL PRIMARY KEY,
+                        benchmark_id INTEGER NOT NULL REFERENCES benchmarks(id) ON DELETE CASCADE,
+                        time_python DOUBLE PRECISION NOT NULL,
+                        time_numba DOUBLE PRECISION NOT NULL
+                    )
+                    """)
+        conn.commit()
     except Exception as error:
         print("Erreur lors de la création de la table", error)
-    conn.commit()
+
+def save_benchmark(image_hash: str, results: dict) -> bool:
+    conn, cur = connect()
+    try:
+        benchmark_id = get_benchmark_id_with_hash_db(image_hash)
+        if not benchmark_id:
+            cur.execute(
+                """
+                INSERT INTO benchmarks (image_hash, image_result)
+                VALUES (%s, %s)
+                RETURNING id
+                """,
+                (image_hash, results.get("image_result"))
+            )
+            benchmark_id = cur.fetchone()["id"]
+
+        for benchmark in results.get("benchmarks", []):
+            cur.execute(
+                """
+                INSERT INTO benchmarks_times (benchmark_id, time_python, time_numba)
+                VALUES (%s, %s, %s)
+                """,
+                (benchmark_id, benchmark.get("python_time"), benchmark.get("numba_time"))
+            )
+        conn.commit()
+        return True
+    except Exception as error:
+        print("Erreur lors de la sauvegarde du benchmark", error)
+        return False
+
+
+def get_benchmark(id: int) -> tuple[Any, ...] | None:
+    conn, cur = connect()
+    try:
+        cur.execute("SELECT * FROM benchmarks WHERE id = %s", (id,))
+        benchmark_infos = cur.fetchone()
+        if not benchmark_infos:
+            return None
+        print(benchmark_infos)
+        cur.execute("SELECT time_python, time_numba FROM benchmarks_times WHERE benchmark_id = %s", (id,))
+        benchmark_infos["benchmark_times"] = cur.fetchall()
+        return benchmark_infos
+    except Exception as error:
+        print("Erreur lors de la récupération du benchmark", error)
+        return None
+
+
+def get_all_benchmarks() -> dict[str, list[Any]] | None:
+    conn, cur = connect()
+    try:
+        cur.execute("SELECT * FROM benchmarks")
+        benchmarks = cur.fetchall()
+
+        results = []
+        for benchmark in benchmarks:
+            cur.execute("SELECT time_python, time_numba FROM benchmarks_times WHERE benchmark_id = %s", (benchmark["id"],))
+            benchmark["benchmark_times"] = cur.fetchall()
+            results.append(benchmark)
+        return { "benchmarks": results }
+    except Exception as error:
+        print("Erreur lors de la récupération des benchmarks", error)
+        return None
+
+
+def get_benchmark_id_with_hash_db(image_hash: str) -> int | None:
+    conn, cur = connect()
+    try:
+        cur.execute("SELECT id FROM benchmarks WHERE image_hash = %s", (image_hash,))
+        result = cur.fetchone()
+        return result["id"] if result else None
+    except Exception as error:
+        print("Erreur lors de la récupération de l'id du benchmark avec le hash", error)
+        return None
+
+def delete_benchmark(id: int) -> bool:
+    conn, cur = connect()
+    try:
+        cur.execute("DELETE FROM benchmarks WHERE id = %s", (id,))
+        conn.commit()
+        return True
+    except Exception as error:
+        print("Erreur lors de la suppression du benchmark", error)
+        return False
 
 def delete_all_benchmarks() -> bool:
     conn, cur = connect()
@@ -46,48 +128,3 @@ def delete_all_benchmarks() -> bool:
     except Exception as error:
         print("Erreur lors de la suppression des benchmarks", error)
         return False
-
-def get_benchmark_if_exist(image_hash: str) -> tuple[Any, ...] | None:
-    conn, cur = connect()
-    try:
-        cur.execute("SELECT * FROM benchmarks WHERE image_hash = %s", (image_hash,))
-        return cur.fetchone()
-    except Exception as error:
-        print("Erreur lors de la vérification du benchmark", error)
-    return None
-
-def save_benchmark(image_hash: str, results: dict) -> bool:
-    conn, cur = connect()
-    #try:
-    cur.execute(
-        """
-        INSERT INTO benchmarks (image_hash, image_result)
-        VALUES (%s, %s)
-        """,
-        (image_hash, results.get("image_result"))
-    )
-    cur.execute("SELECT lastval()")
-    benchmark_id = cur.fetchone()["id"]
-    for benchmark in results.get("benchmarks", []):
-        print(benchmark)
-        cur.execute(
-            """
-            INSERT INTO benchmarks_times (benchmark_id, time_python, time_numba)
-            VALUES (%s, %s, %s)
-            """,
-            (benchmark_id, benchmark.get("python_time"), benchmark.get("numba_time"))
-        )
-    conn.commit()
-    return True
-    #except Exception as error:
-        #    print("Erreur lors de la sauvegarde du benchmark", error)
-    #     return False
-
-def get_all_benchmarks() -> list[dict] | None:
-    conn, cur = connect()
-    try:
-        cur.execute("SELECT * FROM benchmarks JOIN benchmarks_times ON benchmarks.id = benchmarks_times.benchmark_id")
-        return cur.fetchall()
-    except Exception as error:
-        print("Erreur lors de la récupération des benchmarks", error)
-        return None
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,8 @@
 import io, time, hashlib
-from database import create_table, get_benchmark_if_exist, save_benchmark, get_all_benchmarks, delete_all_benchmarks
+
+import numpy as np
+
+from database import create_table, save_benchmark, get_benchmark, get_all_benchmarks, get_benchmark_id_with_hash_db, delete_benchmark, delete_all_benchmarks
 from geodesic_distance import propagation, npropagation
 from imageio.v3 import imread
 from fastapi import FastAPI, UploadFile, File, HTTPException
@@ -9,8 +12,8 @@ app = FastAPI(title="REMPY-IMAGE API")
 create_table()
 
 
-@app.post("/benchmarks")
-async def benchmark(nb_tests: int,
+@app.post("/benchmarks/run")
+async def run_benchmark(nb_tests: int,
                     image: UploadFile = File(...),
                     mask: UploadFile = File()):
     if image.content_type not in {"image/jpeg", "image/png"}:
@@ -20,10 +23,6 @@ async def benchmark(nb_tests: int,
     mask_content = await mask.read()
 
     combined_hash = hashlib.sha256(image_content + mask_content).hexdigest()    # hash des images pour éviter de faire plusieurs fois les benchmarks
-
-    existing = get_benchmark_if_exist(combined_hash)
-    if existing:
-        return {"execution_time": existing["time"] }
 
     image_data = imread(io.BytesIO(image_content))
     mask_data = imread(io.BytesIO(mask_content))
@@ -45,19 +44,52 @@ async def benchmark(nb_tests: int,
             "numba_time": nend_time - nstart_time
         })
 
-    #if not save_benchmark(combined_hash, results):
-    #    raise HTTPException(status_code=500, detail="Erreur lors de la sauvegarde du benchmark")
+    if not save_benchmark(combined_hash, results):
+        raise HTTPException(status_code=500, detail="Erreur lors de la sauvegarde du benchmark")
+
+    results["image_result"] = np.array([]).tolist() ##TODO remove
     return results
 
+@app.post("/benchmark/hash")
+async def get_benchmark_id_with_hash(image: UploadFile = File(...),
+                            mask: UploadFile = File()):
+    if image.content_type not in {"image/jpeg", "image/png"}:
+        raise HTTPException(status_code=400, detail=f"Uploadez seulement des images")
+
+    image_content = await image.read()
+    mask_content = await mask.read()
+
+    combined_hash = hashlib.sha256(image_content + mask_content).hexdigest()    # hash des images pour éviter de faire plusieurs fois les benchmarks
+
+    existing = get_benchmark_id_with_hash_db(combined_hash)
+    if existing:
+        return { "benchmark_id": existing }
+    else:
+        raise HTTPException(status_code=404, detail="Aucun benchmark trouvé pour ces images")
+
+@app.get("/benchmark")
+async def get_benchmark_id(id: int):
+    benchmark = get_benchmark(id)
+    if benchmark is None:
+        raise HTTPException(status_code=500, detail="Erreur lors de la récupération du benchmark")
+    benchmark["image_result"] = np.array([]).tolist() ##TODO remove
+    return benchmark
 
 @app.get("/benchmarks")
-async def list_benchmarks():
+async def get_benchmarks():
     benchmarks = get_all_benchmarks()
     if benchmarks is None:
         raise HTTPException(status_code=500, detail="Erreur lors de la récupération des benchmarks")
+    for benchmark in benchmarks["benchmarks"]:    ##TODO remove
+        benchmark["image_result"] = np.array([]).tolist() ##TODO remove
     return benchmarks
 
 @app.delete("/benchmark")
+async def delete_benchmark_id(id: int):
+    status = delete_benchmark(id)
+    return { "deleted": status }
+
+@app.delete("/benchmarks")
 async def delete_benchmarks():
     status =  delete_all_benchmarks()
     return { "deleted": status }
